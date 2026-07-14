43761c7b670d09d2422bb9b1cc7bd5f0762f909c
AdarshVNair-001 <adarshvnair5000@gmail.com>
2026-07-14 10:02:14 +0530

    created new angular project ShopQ

---
+++ ShopQ/src/app/Components/login-components/login-components.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponentsComponent } from './login-components.component';
+
+describe('LoginComponentsComponent', () => {
+  let component: LoginComponentsComponent;
+  let fixture: ComponentFixture<LoginComponentsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponentsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ShopQ/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponentsComponent } from './login-components/login-components.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
